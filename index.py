a6bf683f9be0b67de2e3e0ecf97c66fc946e3a7c
Gustavinho Programações UAUUUUUUUUUUU <70520011+narasai10@users.noreply.github.com>
2025-01-30 09:42:29 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,47 @@
+import socket
+import pyautogui as pg
+import base64
+
+def salvar_imagem(imagem_base64):
+    try:
+        with open("screenshot.png", "wb") as img_file:
+            img_file.write(base64.b64decode(imagem_base64))
+        print("📸 Screenshot recebido e salvo como screenshot.png!")
+    except Exception as e:
+        print(f"Erro ao salvar imagem: {e}")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 6783))  # Aqui você define o IP e porta
+server_socket.listen(1)
+
+print("Aguardando conexão...")
+client_socket, addr = server_socket.accept()
+print(f"Conexão estabelecida com {addr}")
+
+while True:
+    msg_resposta = input("Digite sua resposta: ")
+    client_socket.send(msg_resposta.encode())
+    if msg_resposta.lower() == "screenshot":
+        imagem_base64 = client_socket.recv(100000000).decode()  # Recebe a string base64 da imagem
+        salvar_imagem(imagem_base64)
+    elif msg_resposta.lower() == "start-control mouse":
+        print("🔄 Controle do mouse ativado!")
+        resposta = client_socket.recv(1024).decode()
+        print(resposta)  # Confirmação do cliente
+
+        while True:
+            acao = input("\nComando do mouse (move x y, click, scroll valor, stop): ")
+
+            if acao.lower() == "stop":
+                client_socket.sendall(acao.encode())
+                print("❌ Controle do mouse desativado.")
+                break  # Sai do modo controle do mouse
+            
+            client_socket.sendall(acao.encode())  # Envia ação para o cliente
+            resposta = client_socket.recv(1024).decode()  # Recebe resposta do cliente
+            print(f"Resposta do cliente: {resposta}")
+    else:
+        resposta = client_socket.recv(4096).decode()
+        print(f"Resposta do cliente:\n{resposta}")
+
+client_socket.close()
